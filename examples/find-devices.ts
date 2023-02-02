d146732aafc49f08b55ab32316c4faa738310a6f
Phil Leggetter <phil@leggetter.co.uk>
2023-02-02 06:58:02 +0000

    chore: use serialNumber in findDevices example

--- examples/find-devices.ts
+++ examples/find-devices.ts
@@ -5,7 +5,9 @@ const devices = findDevices();
 if (devices.length > 0) {
   console.log(`Found ${devices.length} devices connected`);
   for (let i = 0; i < devices.length; ++i) {
-    console.log(`Device ${i + 1}: ${devices[i].type}`);
+    console.log(
+      `Device ${i + 1}: ${devices[i].type} with serial number ${devices[i].serialNumber}`,
+    );
   }
 
   // Do something
